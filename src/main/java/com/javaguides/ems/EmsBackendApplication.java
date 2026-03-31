395f8d2e34f5feb21d2195b5e448bf5b6c20c843
Narasimharao Mannepalli <mannepallinarasimha@gmail.com>
2026-03-31 11:16:58 +0530

    pom.xml and startup application class updated for jenkin pipeline

--- src/main/java/com/javaguides/ems/EmsBackendApplication.java
+++ src/main/java/com/javaguides/ems/EmsBackendApplication.java
@@ -2,9 +2,15 @@ package com.javaguides.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmsBackendApplication {
+public class EmsBackendApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EmsBackendApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmsBackendApplication.class, args);
